f61ebf43c7d3aa54bb2df0a53d68cabe5cb83803
Burak Görez <burakgorez01@gmail.com>
2025-09-24 19:10:11 +0300

    v1

---
+++ calculator.py
@@ -0,0 +1,28 @@
+#Baisc gold calculator with python by Burak "paradass" Görez
+
+import requests
+
+class GoldCalculator:
+    def __init__(self):
+        self.url = "https://malatyakuyumcular.net/"
+    
+    def get_gold_price(self,url):
+        response = requests.get(url,timeout=3)
+        html_code = response.text.split("\n")
+        gram_gold = html_code[135].split(">")[3].split("<")[0]
+        gram_gold = gram_gold.replace(".","")
+        gram_gold = gram_gold.replace(",",".")
+        gram_gold_price = float(gram_gold)
+        return gram_gold_price
+
+    def calculate(self):
+        try:
+            gold_amount = float(input("Kaç gram altının var? "))
+            gram_gold_price = self.get_gold_price(self.url)
+            print(f"{gold_amount} gram altini satarsan \033[32m{gold_amount * gram_gold_price:,.2f}\033[0m₺ ediyor.")
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    gold_calculator = GoldCalculator()
+    gold_calculator.calculate()
